b46cbf0edda3402f3151d7a9436f420ed9b41de0
alber <alber@DESKTOP-FMIF03R>
2021-04-19 15:50:16 -0600

    Programa despliega saludom, primer ejercicio

---
+++ src/ejercicios_github/Ejercicios_github.java
@@ -0,0 +1,21 @@
+/*
+Es un proyecto generado para desarrollar ejercicios que se puedan visualizar en GitHub.
+ */
+package ejercicios_github;
+
+/**
+ *
+ * @author alber
+ */
+public class Ejercicios_github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola soy Jesus Chavez M.");
+    }
+    
+    
+    
+}
